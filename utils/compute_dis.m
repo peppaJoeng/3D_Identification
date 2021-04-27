05a19c5c9b3fac9553076cc0c4510738b973ef56
ysysljjyjq <noblei@163.com>
2021-04-27 19:34:47 +0800

    Update compute_dis.m

--- utils/compute_dis.m
+++ utils/compute_dis.m
@@ -1,8 +1,10 @@
 function distance = compute_dis(rpca)
-%COMPUTE_DIS 此处显示有关此函数的摘要
-%   此处显示详细说明
-dis = rpca(:,1);
-dist = abs(dis);
+%COMPUTE_DIS 姝ゅ鏄剧ず鏈夊叧姝ゅ嚱鏁扮殑鎽樿
+%   姝ゅ鏄剧ず璇︾粏璇存槑
+% dis = rpca(:,1);
+% dist = abs(dis);
+disp('Now compute the mean of the low-rank matrix');
+dist = mean(rpca);
 distance = mean(dist);
 end
 
